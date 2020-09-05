6e22a631c9285106c4b6350b0127220157b922ff
AkshayMule101 <67921032+AkshayMule101@users.noreply.github.com>
2020-09-05 14:21:58 +0530

    Add files via upload

---
+++ src/data_2.js
@@ -0,0 +1,34 @@
+const Promise = require("bluebird");
+const mysql = require("mysql");
+
+// Promisifying the mysql module methods.
+Promise.promisifyAll(require("mysql/lib/Connection").prototype);
+Promise.promisifyAll(require("mysql/lib/Pool").prototype);
+
+let helloDatabase = async () => {
+  const connection = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "abc",
+  });
+
+  // connection open
+  await connection.connectAsync(); // promise
+
+  // let query
+  let sql =
+    "INSERT INTO manager VALUES(?,?,?)";
+  let operation = await connection.queryAsync(sql, [
+    "",
+    "Rohit",
+    "New York",
+  ]);
+
+  // close the connection
+  connection.end();
+
+
+};
+
+helloDatabase();
